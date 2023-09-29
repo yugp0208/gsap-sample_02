5df4bd132f8658a48eaeb638d4494545fd38ba4d
yugp0208 <yuu.fepnhdper2000@gmail.com>
2023-09-29 23:14:57 +0900

    feat vite環境構築

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+//scss
+import '../scss/style.scss'
+
+//Splide
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/css';
+import '@splidejs/splide/css/skyblue';
+import '@splidejs/splide/css/sea-green';
+import '@splidejs/splide/css/core';
+
+new Splide('.splide').mount();
