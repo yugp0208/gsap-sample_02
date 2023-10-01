2cf1c0372a61e5c5e0ad30322803c26968879261
yugp0208 <yuu.fepnhdper2000@gmail.com>
2023-10-01 15:59:54 +0900

    feat テキスト無限ループアニメーション

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,28 @@
 //scss
 import '../scss/style.scss'
+//gsapの読み込み
+import {gsap} from "gsap";
+// gsap.registerPlugin(MotionPathPlugin, TextPlugin,ScrollTrigger);
 
-//Splide
-import Splide from '@splidejs/splide';
-import '@splidejs/splide/css';
-import '@splidejs/splide/css/skyblue';
-import '@splidejs/splide/css/sea-green';
-import '@splidejs/splide/css/core';
+// // 第1引数に対象、第2引数に変化させたいプロパティーと設定値を指定
+// gsap.to(".rect", {scale: 1, duration: 2, ease: "power2.out", y: 300 });
 
-new Splide('.splide').mount();
+// const rect = document.querySelector('.name-wrap');
+// rect.addEventListener('click',function(){
+//     // 繰り返し時に遅延を含める
+//     gsap.to(".name-wrap", {
+//         backgroundColor: "#cccccc",
+//         duration: 1,
+//         repeat:-1,
+//     });
+// })
+
+const name = document.querySelector('.name-wrap');
+console.log(name)
+
+const tl = gsap.timeline({ xPercent :-100,repeat: -1 });
+tl.to(name, {
+    xPercent :100,
+    duration: 3,
+    ease: "none",
+  });
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';//viteの読み込み
 export default defineConfig({
   root: './src/', //開発ディレクトリ設定
   build: {
-    outDir: './dist/', //出力場所の指定
+    outDir: '../dist/', //出力場所の指定
 
     rollupOptions: { //ファイル出力設定
       output: {
